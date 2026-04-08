ce4b09ff9cdfdfdf26723af78ac3af1adec1ff1e
Ava <ava.inroot@gmail.com>
2026-04-08 15:51:03 +0300

    Import Map fix

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Layout } from "./Layout/Layout";
-export { default as Map } from "./Map/Map";
 export { default as Side } from "./Side/Side";
 export { default as Loader } from "./Loader/Loader";
